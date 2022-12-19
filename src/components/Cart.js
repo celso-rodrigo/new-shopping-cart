973444e287a224a25af8424a3235f0dff3f6bae8
Pink <75178570+celsorodrigo@users.noreply.github.com>
2022-12-19 06:26:30 -0300

    Adicionado display de valor ao carrinho.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,13 @@ import Context from "../context/Context";
 import CartItemCard from "./CartItemCard";
 
 function Cart() {
-	const { showCart, cart } = useContext(Context);
+	const { showCart, cart, setCart } = useContext(Context);
+
+	const getTotal = () => {
+		if (!cart.length) return "Cart is empty";
+		if (cart.length === 1) return (cart[0].price * cart[0].quantity).toFixed(2);
+		return cart.reduce((acc, curr) => curr.price * curr.quantity + acc, 0).toFixed(2);
+	};
 
 	return (
 		<aside className={`cart ${showCart ? "" : "cart-hidden"}`}>
@@ -17,6 +23,15 @@ function Cart() {
 					key={item.id}
 				/>
 			))}
+			<p>{`Total: ${getTotal()}`}</p>
+			{cart.length > 0 && (
+				<button
+					type="button"
+					onClick={() => setCart([])}
+				>
+					Clear Cart
+				</button>
+			)}
 		</aside>
   );
 }
