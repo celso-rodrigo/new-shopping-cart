7ded323ae5fbe090e3cb2faec2929749b7779220
Pink <75178570+celsorodrigo@users.noreply.github.com>
2022-12-21 11:53:23 -0300

    Adicionado CSS do header.

--- src/components/StoreHeader.js
+++ src/components/StoreHeader.js
@@ -1,13 +1,28 @@
 import React, { useContext } from "react";
 import Context from "../context/Context";
 import cartIcon from "../images/cart.svg";
+import logoIconFill from "../images/logoIconFill.svg";
 
 function StoreHeader() {
 	const { setShowCart, cart } = useContext(Context);
 
+	const calculateCartItens = () => {
+		const itemAmount = cart.reduce((acc, curr) => acc + curr.quantity, 0);
+		return itemAmount < 100 ? itemAmount : "99+";
+	};
+
   return (
 		<header>
-			<h1 className="title">New Shopping cart</h1>
+			<div className="logo">
+				<div className="logo-image">
+					<img src={logoIconFill} alt="logo." className="logo-icon" />
+					<span className="new">New</span>
+				</div>
+				<div className="title">
+					<h1 className="shopping">Shopping</h1>
+					<h1 className="cart-title">Cart</h1>
+				</div>
+			</div>
 
 			<button
 				type="button"
@@ -16,7 +31,7 @@ function StoreHeader() {
 			>	
 				<div>
 					{cart.length > 0 && <span className="items">
-						{cart.reduce((acc, curr) => acc + curr.quantity, 0)}
+						{calculateCartItens()}
 					</span>}
 					<img src={cartIcon} alt="Cart." className="cart-icon" />
 				</div>
